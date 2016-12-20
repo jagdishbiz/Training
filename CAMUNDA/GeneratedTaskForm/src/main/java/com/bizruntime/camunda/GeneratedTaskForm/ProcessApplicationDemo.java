70fdd1be20b6f9d8790afe81d048ac1f9749edc5
jagdishbiz <jagdish.khetre@bizruntime.com>
2016-12-20 23:08:00 +0530

    Camunda Training

---
+++ CAMUNDA/GeneratedTaskForm/src/main/java/com/bizruntime/camunda/GeneratedTaskForm/ProcessApplicationDemo.java
@@ -0,0 +1,10 @@
+package com.bizruntime.camunda.GeneratedTaskForm;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication
+public class ProcessApplicationDemo extends ServletProcessApplication
+{
+	
+}
---
+++ CAMUNDA/LoanApprovalApplication/src/main/java/com/bizruntime/camunda/LoanApprovalApplication/LoanApprovalApplication.java
@@ -0,0 +1,10 @@
+package com.bizruntime.camunda.LoanApprovalApplication;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication
+public class LoanApprovalApplication extends ServletProcessApplication
+{
+    
+}
